daf75f5e4fc60e03c7f6c35090f138a97db71853
dysunair-tech <dys.unair@gmail.com>
2026-07-08 07:05:17 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "alokasi-v2";
+const CACHE = "alokasi-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
